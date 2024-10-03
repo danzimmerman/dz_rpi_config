917bb843d0c76fa5adc9fe95688c19ec4354c685
Dan Zimmerman <code@danzimmerman.com>
2024-10-02 23:53:36 -0400

    Add setup script for bash config

--- modify_bashrc.sh
+++
@@ -1,6 +0,0 @@
-# file to modify .bashrc to point at this repo
-
-if [ ! -f ~/.bashrc.original ]; then
-    cp ~/.bashrc ~/.bashrc.original
-    sudo chmod a-w ~/.bashrc.original
-fi
---
+++ set_up_bash_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# file to modify .bashrc to point at this repo
+
+if [[ ! -f ~/.bashrc.original && -f ~/dz_rpi_config/bash_config/.bashrc_additions ]]; then
+    echo "Making a read-only copy of ~/.bashrc to ~/.bashrc.original"
+    cp ~/.bashrc ~/.bashrc.original
+    sudo chmod a-w ~/.bashrc.original
+    cat ~/dz_rpi_config/bash_config/.bashrc_mod_tail >> .bashrc
+else
+    echo "~/.bashrc.original backup copy already exists. Remove it if this is not expected."
+fi
+
+if [[ -d ~/dz_rpi_config && ! -f ~/.inputrc && ! -f ~/.bash_aliases ]]; then
+  echo "~/dz_rpi_config exists but ~/.inputrc and ~/.bash_aliases do not, making softlinks..."
+  ln -s ~/dz_rpi_config/bash_config/.bash_aliases ~/.bash_aliases
+  ln -s ~/dz_rpi_config/bash_config/.inputrc ~/.inputrc
+else
+  echo "Attempting to softlink ~/.bash_aliases and ~/.inputrc but one of them was found. Remove them if this is not expected."
+fi

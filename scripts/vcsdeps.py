7ad8432871976ea48d1606c6a42f91a8a2e23fe8
Dan Zimmerman <code@danzimmerman.com>
2024-10-28 09:04:55 -0400

    Formatting + change to test prs pop

--- scripts/vcsdeps.py
+++ scripts/vcsdeps.py
@@ -12,56 +12,54 @@ print(f"***Looking for {ros_distro} package repos at\n\t{rd_yaml_url}")
 
 req = requests.get(rd_yaml_url)
 with io.StringIO(req.text) as ymf:
-  all_repo_info = yaml.safe_load(ymf)
+    all_repo_info = yaml.safe_load(ymf)
 
 # get a lookup map from package -> repository when available
 package_lookup = {}
 for repo_key, repo_info in all_repo_info["repositories"].items():
-  #print(repo_info)
-  try:
-    relinfo = repo_info["release"]
-    if "packages" in relinfo.keys():
-      for pkg in relinfo["packages"]:
-        package_lookup[pkg] = repo_key
-    else:
-      package_lookup[repo_key] = repo_key
-  except KeyError as e:
-    pass
-  
-    
+    # print(repo_info)
+    try:
+        relinfo = repo_info["release"]
+        if "packages" in relinfo.keys():
+            for pkg in relinfo["packages"]:
+                package_lookup[pkg] = repo_key
+        else:
+            package_lookup[repo_key] = repo_key
+    except KeyError as e:
+        pass
+
+
 system_packages = []
 ros_packages = {}
 ros_repo_data = {}
 for pkg in packages:
-  if pkg in package_lookup.keys():
-    repo_key = package_lookup[pkg]
-    # don't duplicate already
-    if not repo_key in ros_repo_data.keys():
-        #print(repo_key)
-        src_data = all_repo_info["repositories"][repo_key]["source"]
-        if "test_pull_requests" in src_data.keys():
-            _ = src_data.pop("test_pull_requests")
-        ros_repo_data[f"src/{repo_key}"] = src_data
-    
-    # keep a list of packages that comes from each repo
-    if not repo_key in ros_packages.keys():
-        ros_packages[repo_key] = []
-        ros_packages[repo_key].append(pkg) 
-    else:
-      ros_packages[repo_key].append(pkg)
+    if pkg in package_lookup.keys():
+        repo_key = package_lookup[pkg]
+        # don't duplicate repositories we already added
+        if not repo_key in ros_repo_data.keys():
+            # print(repo_key)
+            src_data = all_repo_info["repositories"][repo_key]["source"]
+            try:
+                _ = src_data.pop("test_pull_requests")
+            except:
+                pass
+            ros_repo_data[f"src/{repo_key}"] = src_data
 
-  else:
-    system_packages.append(pkg)
+        # keep a list of packages that comes from each repo
+        if not repo_key in ros_packages.keys():
+            ros_packages[repo_key] = []
+            ros_packages[repo_key].append(pkg)
+        else:
+            ros_packages[repo_key].append(pkg)
+
+    else:
+        system_packages.append(pkg)
 
 
 repos_filename = pathlib.Path("./ws_deps.repos").absolute()
 print(f"Writing {repos_filename.as_posix()}")
 with repos_filename.open("w") as rpf:
-    rpf.write(
-    yaml.dump(
-        {"repositories":ros_repo_data}
-    )
-)
+    rpf.write(yaml.dump({"repositories": ros_repo_data}))
 
 print("System Package Installation")
 pkgstr = " ".join(system_packages)
